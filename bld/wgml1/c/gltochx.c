1f067bdf731f622191a793fc17297c8350cf62fc
Jiri Malak <malak.jiri@gmail.com>
2025-04-03 19:15:24 +0200

    wgml: clenup copyright data, only OW contributors

--- bld/wgml1/c/gltochx.c
+++ bld/wgml1/c/gltochx.c
@@ -1,26 +1,8 @@
 /****************************************************************************
 *
-*                            Open Watcom Project
+*                           Open Watcom Project
 *
-*  Copyright (c) 2004-2009 The Open Watcom Contributors. All Rights Reserved.
-*
-*  ========================================================================
-*
-*    This file contains Original Code and/or Modifications of Original
-*    Code as defined in and that are subject to the Sybase Open Watcom
-*    Public License version 1.0 (the 'License'). You may not use this file
-*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
-*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
-*    provided with the Original Code and Modifications, and is also
-*    available at www.sybase.com/developer/opensource.
-*
-*    The Original Code and all software distributed under the License are
-*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
-*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
-*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
-*    NON-INFRINGEMENT. Please see the License for the specific language
-*    governing rights and limitations under the License.
+* Copyright (c) 2004-2025 The Open Watcom Contributors. All Rights Reserved.
 *
 *  ========================================================================
 *
@@ -175,7 +157,7 @@ void    lay_tochx( const gmltag * entry )
                 switch( curr ) {
                 case   e_group:
                     if( AttrFlags.group ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_int8( p, curr, &layout_work.tochx[hx_l].group );
@@ -183,7 +165,7 @@ void    lay_tochx( const gmltag * entry )
                     break;
                 case   e_indent:
                     if( AttrFlags.indent ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_space_unit( p, curr,
@@ -192,7 +174,7 @@ void    lay_tochx( const gmltag * entry )
                     break;
                 case   e_skip:
                     if( AttrFlags.skip ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_space_unit( p, curr,
@@ -201,7 +183,7 @@ void    lay_tochx( const gmltag * entry )
                     break;
                 case   e_pre_skip:
                     if( AttrFlags.pre_skip ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_space_unit( p, curr,
@@ -210,7 +192,7 @@ void    lay_tochx( const gmltag * entry )
                     break;
                 case   e_post_skip:
                     if( AttrFlags.post_skip ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_space_unit( p, curr,
@@ -219,7 +201,7 @@ void    lay_tochx( const gmltag * entry )
                     break;
                 case   e_font:
                     if( AttrFlags.font ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_font_number( p, curr, &layout_work.tochx[hx_l].font );
@@ -230,7 +212,7 @@ void    lay_tochx( const gmltag * entry )
                     break;
                 case   e_align:
                     if( AttrFlags.align ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_space_unit( p, curr,
@@ -239,7 +221,7 @@ void    lay_tochx( const gmltag * entry )
                     break;
                 case   e_display_in_toc:
                     if( AttrFlags.display_in_toc ) {
-                        xx_line_err_ci( err_att_dup, g_att_val.att_name, 
+                        xx_line_err_ci( err_att_dup, g_att_val.att_name,
                             g_att_val.val_name - g_att_val.att_name + g_att_val.val_len);
                     }
                     cvterr = i_yes_no( p, curr,
--- bld/wgml1/c/gsmacro.c
+++ bld/wgml1/c/gsmacro.c
@@ -1,26 +1,8 @@
 /****************************************************************************
 *
-*                            Open Watcom Project
+*                           Open Watcom Project
 *
-*  Copyright (c) 2004-2009 The Open Watcom Contributors. All Rights Reserved.
-*
-*  ========================================================================
-*
-*    This file contains Original Code and/or Modifications of Original
-*    Code as defined in and that are subject to the Sybase Open Watcom
-*    Public License version 1.0 (the 'License'). You may not use this file
-*    except in compliance with the License. BY USING THIS FILE YOU AGREE TO
-*    ALL TERMS AND CONDITIONS OF THE LICENSE. A copy of the License is
-*    provided with the Original Code and Modifications, and is also
-*    available at www.sybase.com/developer/opensource.
-*
-*    The Original Code and all software distributed under the License are
-*    distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-*    EXPRESS OR IMPLIED, AND SYBASE AND ALL CONTRIBUTORS HEREBY DISCLAIM
-*    ALL SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF
-*    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR
-*    NON-INFRINGEMENT. Please see the License for the specific language
-*    governing rights and limitations under the License.
+* Copyright (c) 2004-2025 The Open Watcom Contributors. All Rights Reserved.
 *
 *  ========================================================================
 *
@@ -82,7 +64,7 @@ static char *verify_sym( char * p )
             }
         }
     }
-        
+
     return( pa );
 }
 
@@ -539,14 +521,14 @@ void    scr_dm( void )
                 p++;
 
                 /****************************************************************/
-                /* although ".." is used with DM in the OW Docs, '.'" is not    */ 
-                /* constructs like "..'" ".'." have yet to be explored          */ 
+                /* although ".." is used with DM in the OW Docs, '.'" is not    */
+                /* constructs like "..'" ".'." have yet to be explored          */
                 /****************************************************************/
 
                 if( (*p == SCR_char)  || (*p == '\'') ) {
                     p++;                        // over ".." or ".'"
                 }
-                while( len < MAC_NAME_LENGTH ) { 
+                while( len < MAC_NAME_LENGTH ) {
                     if( is_space_tab_char( *p ) || (*p == '\0') ) { // largest possible macro/cw
                         break;
                     }
